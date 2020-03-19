bb4795639b37658de026725fd49a9bc7828474b0
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-19 16:01:15 -0600

    Improve test to be more dynamic, remove unnecessary test aspects

--- spec/features/shelters/visitor_can_see_shelter_info_spec.rb
+++ spec/features/shelters/visitor_can_see_shelter_info_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "shelter id page", type: :feature do
   it "can see all info of individual shelter" do
-    shelter_1 = Shelter.create(id:  1,
-                               name:  "Dumb Friends League",
+    shelter_1 = Shelter.create(name:  "Dumb Friends League",
                                address: "2080 S Quebec St",
                                city:   "Denver",
                                state:  "CO",
                                zip:  "80231")
 
-    visit '/shelters/id'
+    visit "/shelters/#{shelter_1.id}"
 
-    expect(page).to have_content(shelter_1.id)
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_1.address)
     expect(page).to have_content(shelter_1.city)
