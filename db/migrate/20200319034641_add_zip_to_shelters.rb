d43f2b6cec6a632ebd3c9123bcb7b405b1883158
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-19 15:58:14 -0600

    Combine migrartions in one file

--- db/migrate/20200319034641_add_zip_to_shelters.rb
+++
@@ -1,5 +0,0 @@
-class AddZipToShelters < ActiveRecord::Migration[5.1]
-  def change
-    add_column :shelters, :zip, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200319034641) do
+ActiveRecord::Schema.define(version: 20200318075044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "address"
     t.string "city"
     t.string "state"
     t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
