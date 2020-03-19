a78903ed58e638e50b14a12ea9861beff9977213
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-19 17:48:13 -0600

    Create test for testing new shelter link functionality

---
+++ spec/features/shelters/visitor_can_create_new_shelter_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "new shelter" do
+  describe "as a visitor" do
+    describe "when i visit the new shelter form by clicking a link on the index" do
+      it "i can create a new shelter" do
+
+        visit "/shelters"
+
+        click_link "New Shelter"
+
+        fill_in "Name", with: "Denver Cat Company"
+
+        expect(current_path).to ep ("artists/new")
+
+        click_on "Create Shelter"
+
+        expect(current_path).to eq("/shelters")
+        expect(page).to have_content("Denver Cat Company")
+      end
+    end
+  end
+end
