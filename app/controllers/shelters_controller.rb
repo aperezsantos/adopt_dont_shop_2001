77e815e768becc3e1bc52c29f02fb63320c4ee55
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-19 21:13:10 -0600

    Include route for creating new shelter, add create and shelter_params actions

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -6,7 +6,18 @@ class SheltersController < ApplicationController
   def new
   end
 
+  def create
+    Shelter.create(shelter_params)
+    redirect_to '/shelters'
+  end
+
   def show
     @shelter = Shelter.find(params[:id])
   end
+  
+  private
+  def shelter_params
+      params.permit(:name)
+  end
+
 end
