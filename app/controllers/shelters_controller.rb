47e6944b0d2c2d63a6c0f4f38de55d51d83e0014
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-21 02:14:10 -0600

    Create edit action and define its functionality

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -15,6 +15,10 @@ class SheltersController < ApplicationController
     redirect_to "/shelters"
   end
 
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
 
   private
     def shelter_params
