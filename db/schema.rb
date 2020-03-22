0ffe327d71dae3b95ac8cb179d169a37b1728c74
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-22 01:45:48 -0600

    Ran a migration to add the pets table with the shelters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200322013848) do
+ActiveRecord::Schema.define(version: 20200322065421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20200322013848) do
     t.string "approximate_age"
     t.string "sex"
     t.string "shelter_location"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 20200322013848) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pets", "shelters"
 end
