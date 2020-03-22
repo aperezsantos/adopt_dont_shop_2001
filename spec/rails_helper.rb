823ea7d5fc9ed21842b0d80e527949e743fdad15
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-22 15:27:34 -0600

    Add gem list

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,7 +59,8 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  config.filter_gems_from_backtrace("capybara-3.29.0", "rack-2.0.7", "rack-test-1.1.0", "railties-5.1.7")
+
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
