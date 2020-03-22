f200b98d4939f221557ddce2ad9027fe0a8755f3
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-22 00:06:55 -0600

    Add missing shelter information

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -6,15 +6,22 @@ RSpec.describe "shelter index page", type: :feature do
     describe "i visit a shelter index page" do
       it "i can see all shelter names in the system" do
 
-      shelter_1 = Shelter.create(name: "Dumb Friends League")
-      shelter_2 = Shelter.create(name: "Cat Care Society")
-      shelter_3 = Shelter.create(name: "Foothills Animal Shelter")
+      shelter_1 = Shelter.create(name:  "Dumb Friends League",
+                                  address: "2080 S Quebec St",
+                                  city:   "Denver",
+                                  state:  "CO",
+                                  zip:  "80231")
+
+      shelter_2 = Shelter.create(name: "Cat Care Society",
+                                 address:  "5787 W 6th Ave",
+                                 city: "Lakewood",
+                                 state:  "CO",
+                                 zip:  "80214")
 
       visit '/shelters'
 
       expect(page).to have_content(shelter_1.name)
       expect(page).to have_content(shelter_2.name)
-      expect(page).to have_content(shelter_3.name)
       end
     end
   end
